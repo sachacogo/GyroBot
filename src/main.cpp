3db214ccadc610730639e33741baa63866524c5c
Sacha COGO <sachacogo@gmail.com>
2025-08-17 16:16:22 +0200

    initialisation

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,24 @@
-#include <Arduino.h>
-
-// put function declarations here:
-int myFunction(int, int);
-
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
-}
-
-void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
+// User led
+#define LEDU1 25
+#define LEDU2 26
+// Enable moteurs droit et gauche
+#define EN_D 23
+#define EN_G 4
+// Commande PWM moteur droit
+#define IN_1_D 19
+#define IN_2_D 18
+// Commande PWM moteur gauche
+#define IN_1_G 17
+#define IN_2_G 16
+// Encodeur gauche
+#define ENC_G_CH_A 32
+#define ENC_G_CH_B 33
+// Encodeur droit
+#define ENC_D_CH_A 27
+#define ENC_D_CH_B 14
+// I2C
+#define SDA 21
+#define SCL 22
+// Adresse I2C
+#define ADDR_IMU 0x6B
+#define ADDR_MAG 0x1E
